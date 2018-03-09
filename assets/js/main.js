03016b6a7573ea1bd92620353fe3c4dda58af250
Kyliekyler <32427049+Kyliekyler@users.noreply.github.com>
2018-03-10 01:04:11 +0800

    Update
    Remove lighting effect

--- assets/js/main.js
+++ assets/js/main.js
@@ -120,13 +120,6 @@
     window.onunload=function(){null};
     
 /* ios 10 style shadow */
-    
-    $(document).on('mousemove', function(e){
-    $('.light').css({
-       left:  e.pageX - 300,
-       top:   e.pageY - 300
-    });
-});
 
 var el = $('.js-tilt-container');
 
