b7cf09a356b53ec4c3b22a85d59deba3d3f683b8
Kyliekyler <32427049+Kyliekyler@users.noreply.github.com>
2018-03-09 23:38:53 +0800

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -150,3 +150,13 @@ $(this).removeClass('leave');
 el.on('mouseleave', function(){
 	$(this).addClass('leave');
 });
+
+/* fb comment plugin */
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.12&appId=793186144175539&autoLogAppEvents=1';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
