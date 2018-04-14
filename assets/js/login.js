3e95f7cc1bc4b329d52f9932a486dcf88af12ed2
Kyliekyler <32427049+Kyliekyler@users.noreply.github.com>
2018-04-14 15:16:54 +0800

    Update

--- assets/js/login.js
+++ assets/js/login.js
@@ -2,21 +2,21 @@ var attempt = 3; //Variable to count number of attempts
 
 //Below function Executes on click of login button
 function validate(){
-	var username = document.getElementById("username").value;
+	var email = document.getElementById("email").value;
 	var password = document.getElementById("password").value;
 
-	if ( username == "kyliekyler" && password == "kyliekyler"){
+	if ( email == "siosonkylebrandon@gmail.com" && password == "Sioson19"){
 		alert ("Login successfully");
 		window.location = "3zh-rR7-rZ8-dBL.html"; //redirecting to other page
 		return false;
 	}
 	else{
 		attempt --;//Decrementing by one
-		alert("You have left "+attempt+" attempt;");
+		alert("You have "+attempt+" attempt(s) left");
 		
 		//Disabling fields after 3 attempts
 		if( attempt == 0){
-			document.getElementById("username").disabled = true;
+			document.getElementById("email").disabled = true;
 			document.getElementById("password").disabled = true;
 			document.getElementById("submit").disabled = true;
 			return false;
